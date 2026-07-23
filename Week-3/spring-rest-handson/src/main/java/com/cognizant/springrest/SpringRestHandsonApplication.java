4f99444c5d078ded0a90274746a4fc3e0c62fba0
06krish <143773425+06krish@users.noreply.github.com>
2026-07-23 11:10:49 +0530

    REST - Country Web Service

--- Week-3/spring-rest-handson/src/main/java/com/cognizant/springrest/SpringRestHandsonApplication.java
+++ Week-3/spring-rest-handson/src/main/java/com/cognizant/springrest/SpringRestHandsonApplication.java
@@ -6,8 +6,10 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:country.xml")
 public class SpringRestHandsonApplication {
 	private static final Logger LOGGER = LoggerFactory.getLogger(SpringRestHandsonApplication.class);
 
