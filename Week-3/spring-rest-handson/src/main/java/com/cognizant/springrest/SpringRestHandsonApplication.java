23de1fb855e2cde0bf1ccef6e0a4b3ce53844002
06krish <143773425+06krish@users.noreply.github.com>
2026-07-23 11:08:37 +0530

    Spring Core – Load Country from Spring Configuration XML

---
+++ Week-3/spring-rest-handson/src/main/java/com/cognizant/springrest/SpringRestHandsonApplication.java
@@ -0,0 +1,26 @@
+package com.cognizant.springrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SpringBootApplication
+public class SpringRestHandsonApplication {
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringRestHandsonApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestHandsonApplication.class, args);
+		displayCountry();
+	}
+
+	public static void displayCountry() {
+		LOGGER.info("START");
+		ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+		Country country = context.getBean("country", Country.class);
+		LOGGER.debug("Country : {}", country);
+		LOGGER.info("END");
+	}
+}
